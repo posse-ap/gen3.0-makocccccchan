7224f1a4a9e8c60847468e78ad172714646ebf31
Makoto Jimbo <makotoda4649@gmail.com>
2022-11-27 10:50:52 +0900

    わーい

--- ph1-webapp/assets/scripts/modal.js
+++ ph1-webapp/assets/scripts/modal.js
@@ -1,25 +1,12 @@
 'use strict'
 
+
 {
+  console.log("hello");
 
-  // to-modalLoadingと言うidをもつDOMを取得する
-  const toModalLoading = document.getElementById('to-modalLoading');
+  
 
-  // toModalLoadingがclickイベントを拾えるようにする
-  toModalLoading.addEventListener('click', ()=>{
-      // 投稿モーダルを非表示にし、ローディングモーダルを表示する
-      // これも関数に切り出しても良いかもね
-      $('#modalPost').modal('hide');
-      $('#modalLoading').modal('show');
 
-      // 2秒後にtoModalSuccessを呼び出す
-      setTimeout(function(){
-          toModalSuccess();
-      }, 2000)
-  })
 
-  function toModalSuccess(){
-    $('#modalLoading').modal('hide');
-    $('#modalSuccess').modal('show');
-  }
+  
 }
